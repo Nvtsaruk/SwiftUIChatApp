c7e80613af3fc148ebc253a64eaaff67c4f76d56
Nvtsaruk <nvtsaruk@gmail.com>
2024-01-04 13:09:19 +0300

    Initial Commit

---
+++ SwiftUIChatApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIChatApp
+//
+//  Created by Tsaruk Nick on 4.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIChatApp/SwiftUIChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChatAppApp.swift
+//  SwiftUIChatApp
+//
+//  Created by Tsaruk Nick on 4.01.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
